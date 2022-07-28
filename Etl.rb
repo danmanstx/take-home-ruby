437883556f655f0fa936f9b51bf912763e5de3d3
Danny Peters <dpeters@m1max.local>
2022-07-28 00:04:34 -0400

    add logic for formating phone and check in output for it

--- Etl.rb
+++ Etl.rb
@@ -40,12 +40,17 @@ class Etl
 
   def self.failed_check(row)
     return true if check_if_missing_required(row)
+    return true if check_phone(row[:phone_number])
     false
   end
 
   def self.transform_row(row)
     transform_all_date_fields(row)
     row
+  end
+
+  def self.check_phone(phone)
+    phone =~ /\+1\d{10}/ ? false : true
   end
 
   def self.check_if_missing_required(row)
@@ -69,6 +74,19 @@ class Etl
       Date.strptime(date, '%m/%d/%y').iso8601
     rescue Date::Error
       Date.strptime(date, '%m-%d-%y').iso8601
+    end
+  end
+
+  def self.transform_phone(phone_num)
+    return phone_num if phone_num == nil
+    phone_num = phone_num.to_s.gsub(/\D/,'')
+    case phone_num.size
+    when 10
+      "+1" + phone_num
+    when 11
+      "+" + phone_num
+    else
+      nil
     end
   end
 
